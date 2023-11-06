402408a86494f5b5e001d813037fb526a1bc4a7f
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-11-06 09:34:25 +0500

    return the average

--- basic_function_parameter04.py
+++ basic_function_parameter04.py
@@ -1,4 +1,7 @@
 # Create a function named calculate_average that takes a list of numbers as a parameter.
+def calculate_average(list):
 # Inside the function, calculate the average of all the numbers in the given list.
 # Return the average.
 # Return the average.
+    return int(sum(list)/len(list))
+print(calculate_average([85, 90, 92, 88, 95]))
